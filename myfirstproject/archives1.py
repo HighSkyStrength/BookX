725db0f4bdacf327e2da5fd838a55b796441894f
Shiga <75756285+Shiga-Sushii@users.noreply.github.com>
2021-02-17 20:39:10 +0100

    En fait j'essaye lul

--- myfirstproject/archives1.py
+++ myfirstproject/archives1.py
@@ -1,14 +1,7 @@
 
-if __name__ == '__main__':
-    wallet = int(input("Veuiller insérer votre budget"))
-    livre = 50
-    print("Vous avez " + str(wallet) + "€")
-    choice = input("Souhaitez-vous acheter un livre à 50 euros")
-    true = "y"
-    false = "n"
-    if choice == true:
-        past_wallet = wallet - livre
-        print("Vous avez acheté le livre, votre nouveau budget est de " + str(past_wallet) + "€")
-    else:
-        print("Vous avez refusé l'achat")
-
+panier1 = input(" AA ")
+print(panier1)
+panier2 = input("bb ")
+print(panier2)
+panier = panier1 + panier2
+print(panier)
--- myfirstproject/bibliotheka_official.py
+++ myfirstproject/bibliotheka_official.py
@@ -56,9 +56,15 @@ def menu():
             choice = input("->")
     print("Donc vous voulez emprunter un livre...")
     emprunt()
+global fantaisie_list
+global aventure
+global SF
+global BD
+global manga
+global documentaire
+global kids_books
 
 def emprunt():
-    global fantaisie
     print("Quel type de livre voulez-vous ? Choix possibles: Fantaisie, Aventure, SF, BD, Manga, Documaentaire, livre pour enfants.")
     rep_1 = "FANTAISIE"
     rep_2 = "AVENTURE"
@@ -67,7 +73,7 @@ def emprunt():
     rep_5 = "MANGA"
     rep_6 = "DOCUMENTAIRE"
     rep_7 = "LIVRE POUR ENFANTS"
-    fantaisie = ["Begin Fantasy I" , "Water Emblem" , "PhiBos" , "El Doredore"]
+    fantaisie_list = ["Begin Fantasy I" , "Water Emblem" , "PhiBos" , "El Doredore"]
     aventure = ["Pékomon" , "Les Aventures de,Mi-loup et Thym-Thym"]
     SF = ["20 milles endroits sur la terre" , "Voyage à la ferme pas loin d'ici"]
     BD = ["Les Aventures de Mi-loup et Thym-Thym" , "Les Légendaire pas trop non plus"]
@@ -81,7 +87,7 @@ def emprunt():
     if answer.upper() == rep_1:
         answer = input("Vous voulez donc des livres de style fantaisie ? [y/n]")
         if answer.upper() == "Y":
-            f()
+            fantaisie()
         elif answer.upper() == "N":
             emprunt()
     if answer.upper() == rep_2:
@@ -120,17 +126,26 @@ def emprunt():
                 print(kids_books)
             elif answer.upper() == "N":
                 emprunt()
-def f() : 
-    print(fantaisie)
-    choix_book_fantaisie = input("Quel livre voulez-vous choisir ?")
-    if choix_book_fantaisie.upper() == "BEGIN FANTASY I" : 
-        choix = input("Prendre 'Begin Fantasy I' comme emprunt ? ")
-        if choix.upper() == "Y" : 
-            panier()
-        elif choix.upper() == "N" :
-            fantaisie()
-
+def fantaisie() : 
+    global panier
+    print(fantaisie_list)
+    choix = input("Vous êtes sur la catégorie 'FANTAISIE', si vous voulez retourner au menu des types, tapez 'menu', si vous voulez continuer votre emprunt, tapez 'emprunt' ")
+    if choix.upper == "MENU" :
+        emprunt()
+    elif choix.upper == "EMPRUNT" :
+        choix_book_fantaisie = input("Quel livre voulez-vous choisir ?")
+        if choix_book_fantaisie.upper() == "BEGIN FANTASY I" : 
+            choice = input("Prendre 'Begin Fantasy I' comme emprunt ? ")
+            if choix.upper() == "Y" :
+                panier1 = "Begin Fantasy I" 
+                retour = input("Continuer vos achats ?")
+                if retour.upper == "Y":
+                    fantaisie()
+                elif retour.upper == "N" :
+                    panier()
+            elif choix.upper() == "N" :
+                fantaisie()
 def panier():
     print("Panier à choux")
 
-emprunt()
+fantaisie()
